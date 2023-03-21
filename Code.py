095b0e17bdb5576101e8487679f2ac6fbebc6d7a
JRay9487 <65828051+JRay9487@users.noreply.github.com>
2023-03-21 19:18:35 +0800

    Update Code.py
    Alpha 1.0

--- Code.py
+++ Code.py
@@ -1,7 +1,46 @@
+# -*- coding: UTF-8 -*-
+#Code By JRay 20230321
+
+import os 
+import subprocess
 import datetime
 
+#Variable
+web_adr = str("www.google.com")
+packet_amount = str(input("Packet Amount: "))
+packet_size = str(32)
+str_amount = str(0)
+path = os.getcwd() +"\Log"
+
+#Date
 date = datetime.datetime.now()
 datetime_format = date.strftime("%Y%m%d_%H%M")
-file_name = "Log_" + datetime_format
 
-print(file_name)
+#Log_dir
+if not os.path.isdir(path):
+    os.makedirs(path, mode=0o777) 
+
+#Export
+file_name = "Log_" + datetime_format + ".txt"
+file_position = path+ "\\" +file_name
+
+#Log
+with open(file_position, "w") as file:
+    file.write('---------------------------------------------------------------\n')
+    file.write("Date: "+ str(date))
+    file.write("\nWeb_address: " + web_adr +
+      "\nPacket_amount: "+ packet_amount +
+      "\nPacket_size: "+ packet_size +
+      "\nStress_amount: "+ str_amount
+      )
+    file.write('\n---------------------------------------------------------------\n')
+
+#Main
+cmd = "ping " + web_adr + " -n " + packet_amount + " -l " + packet_size
+context = open(file_position, "a") 
+subprocess.run(cmd, stdout=context) 
+
+
+
+
+print(path)
